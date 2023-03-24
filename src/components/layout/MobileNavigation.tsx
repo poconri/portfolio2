ff9b8885d975d7e058af685c1747913c929fd4d1
Ramon Pocon <pocon.ri@gmail.com>
2023-03-23 21:48:14 -0600

    fix menu.

--- src/components/layout/MobileNavigation.tsx
+++ src/components/layout/MobileNavigation.tsx
@@ -134,7 +134,7 @@ const MobileNavigation = ({ changeState }: MobileNavigationProps) => {
               </ScrollLink>
             )}
           </li>
-          <li className="block">
+          {/* <li className="block">
             <Link href="/posts/1">
               <a
                 className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
@@ -144,7 +144,7 @@ const MobileNavigation = ({ changeState }: MobileNavigationProps) => {
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
               </a>
             </Link>
-          </li>
+          </li> */}
           <li className="block">
             {checkroute ? (
               <Link href="/">
