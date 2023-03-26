c2ee282e03f8c4cdfc233240b098dc994f8293f9
Ramon Pocon <pocon.ri@gmail.com>
2023-03-26 16:55:00 -0600

    WIP.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Homepage from "./homepage";
-import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
-import { Analytics, getAnalytics } from "firebase/analytics";
-import { Firestore, getFirestore } from "firebase/firestore";
 
 function index() {
   return (
